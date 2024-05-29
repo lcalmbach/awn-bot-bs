70ba5fbbba5c1dfa761760761b9303e222277567
Lukas Calmbach <lcalmbach@gmail.com>
2024-05-29 15:36:20 +0200

    Daten frisch geladen

--- initial_data.py
+++ initial_data.py
@@ -1,6 +1,10 @@
 import pandas as pd
-from const import ADR_FILE, APARTMENT_FILE
+import requests
+import io
 
+PROXY_DICT = {"https": "http://dpdstatasvsql05:dpdstatasvsql05_@proxy1.bs.ch:3128"}
+ADR_FILE = "data/entries.parquet"
+APARTMENT_FILE = "data/apartments.parquet"
 URL_TEMPLATE = (
     "https://data.bs.ch/api/explore/v2.1/catalog/datasets/{}/exports/csv?"
     "lang=de&timezone=Europe%2FBerlin&use_labels=false&delimiter=%3B&select={}"
@@ -21,6 +25,10 @@ fields = ",".join(
     ]
 )
 url_apartments = URL_TEMPLATE.format(100232, fields)
+
+r = requests.get(url_apartments, proxies=PROXY_DICT).text
+
+# apa_df = pd.read_csv(io.StringIO(r), sep=";")
 apa_df = pd.read_csv(url_apartments, sep=";")
 apa_df.to_parquet(
     APARTMENT_FILE,
@@ -42,6 +50,7 @@ adr_df["latitude"] = adr_df["latitude"].astype(float)
 adr_df["longitude"] = adr_df["longitude"].astype(float)
 adr_df.to_parquet(ADR_FILE, engine="pyarrow")
 
+print("Download completed")
 
 # print(adr_df.head())
 # streets = adr_df['strname'].unique()
